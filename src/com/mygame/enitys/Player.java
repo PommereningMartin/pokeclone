e2e9660f1447d4928b13419fe66dbdd914e9475e
Martin Pommerening <martin.pommerening@fh-erfurt.de>
2019-12-12 19:28:41 +0100

    add attributes like velocity and health

--- src/com/mygame/enitys/Player.java
+++ src/com/mygame/enitys/Player.java
@@ -7,7 +7,10 @@ package com.mygame.enitys;
  */
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 /**
@@ -24,38 +27,50 @@ public class Player extends Entity{
 	};
 	private static final long serialVersionUID = 1L;
 	private Rectangle playerImage;
+	private Texture playerTexture;
+	private int health;
+	private String playerName;
     
     public Player() {
         this.playerImage = new Rectangle();
         playerImage.setSize(50.0f, 50.0f);
     }
     
-    public void render() {
-    }
-
-	public boolean collidesLeft() {
-		// TODO Auto-generated method stub
-		return false;
+	public void draw(SpriteBatch _spriteBatch){
+		playerTexture = new Texture("entity/Java2D_Player_Sprite.png");
+		_spriteBatch.draw(playerTexture, 192f, 224f, 16, 16);
+	}
+	
+	public void draw(){
+		
 	}
 
-	public boolean collidesRight() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public void update(float deltaTime) {
 
-	public boolean collidesBottom() {
-		// TODO Auto-generated method stub
-		return false;
 	}
-
-	public boolean isCellDoor(float x, float y) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public String getPlayerName()
+	{
+		return this.playerName;
 	}
-
-	public Velocity getVelocity() {
-		// TODO Auto-generated method stub
-		return Velocity.x;
+	
+	public int getHealth()
+	{
+		return this.health;
 	}
+	
+	public final Vector2 getVelocity(){return velocity;}
+	
+	public final void setVelocityY(float _y){
+		velocity.y = _y;
+	}
+	
+	public final void setVelocityX(float _x){
+		velocity.x = _x;
+	}
+	
+	public final void setTexture(String _pathToTexture){
+		playerTexture = new Texture(_pathToTexture);
+		}
     
 }
